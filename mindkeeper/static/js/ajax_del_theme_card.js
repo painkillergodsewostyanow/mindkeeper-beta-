039e548067d1a142458d042d0db6ba26a99f08fc
painkillergod <thebladethatbringsdeath@gmail.com>
2023-08-12 23:26:06 +0300

    fixes

--- mindkeeper/static/js/ajax_del_theme_card.js
+++ mindkeeper/static/js/ajax_del_theme_card.js
@@ -1,18 +1,35 @@
 
 // init triggers
-let del_triggers = document.querySelectorAll('#del_obj')
+let del_theme_triggers = document.querySelectorAll('#del_theme')
+let del_card_triggers = document.querySelectorAll('#del_card')
 
-for (i=0; i<del_triggers.length; i++){
 
-    del_triggers[i].onclick = del_theme
+for (i=0; i<del_theme_triggers.length; i++){
+
+    del_theme_triggers[i].onclick = del_theme
 
 }
+
+for (i=0; i<del_card_triggers.length; i++){
+
+    del_card_triggers[i].onclick = del_card
+
+}
+
+
 
 // functions
 function del_theme(){
 
     url = this.getElementsByTagName('a')[0].href
     ajax_del_theme(url)
+
+}
+
+function del_card(){
+
+    url = this.getElementsByTagName('a')[0].href
+    ajax_del_card(url)
 
 }
 
@@ -29,11 +46,29 @@ function ajax_del_theme(url) {
 	    .then(json => render_del_theme(json))
 };
 
+function ajax_del_card(url) {
+    fetch(url, {
+		method: 'DELETE',
+		headers : {
+		    'X-CSRFToken': csrf_token,
+		}
+	})
+	    .then(response => response.json())
+	    .then(json => render_del_card(json))
+};
+
 //render functions
 
 function render_del_theme(data){
 
-       let card = document.getElementById(data.deleted_objs[0].pk)
+       let card = document.getElementById(`theme_${data.deleted_objs[0].pk}`)
+       card.remove()
+
+}
+
+function render_del_card(data){
+
+       let card = document.getElementById(`card_${data.deleted_objs[0].pk}`)
        card.remove()
 
 }
